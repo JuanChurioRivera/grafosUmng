e267636f86ee916067b945e2f74339816a3b60dd
JuanChurio <est.juan.churio@unimilitar.edu.co>
2024-03-14 10:15:50 -0500

    trialImages

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { GiHamburgerMenu } from "react-icons/gi";
-import { useState} from'react'
-//Elements of Home page
-//<Route path="/SignUpForm" element={<SignUpForm />} />
-//import SignUpForm from'../src/pages/SignUpForm'
-//import Navbar from'../src/components/Navbar'
-import Sidebar from'../src/components/Sidebar'
-
-//pages
-import Home from'../src/pages/Home'
-import Explorer from'../src/pages/Explorer'
-
-import './App.scss';
+import React from 'react';
+import ImageComponent from './ImageComponent';
 
 function App() {
-  const [showNav, setShownav]= useState(true);
   return (
-    <Router>
-      <header>
-      <div className="iconMods">
-      <GiHamburgerMenu onClick={()=> setShownav(!showNav)}/>
-        </div>
-      </header>
-        <Sidebar show={showNav}/>
-        <div className="aligText">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/Explorer" element={<Explorer />} />
-            </Routes>
-            </div>
-    </Router>
+    <div className="App">
+      <ImageComponent/>
+    </div>
   );
 }
 
--- src/pages/Explorer.jsx
+++
@@ -1,8 +0,0 @@
-const Explorer= ()=>{
-    return (
-        <div className="Explorer">
-            Explorer
-        </div>
-      );
-}
-export default Explorer;
