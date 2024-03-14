738cca61fbffe2d2393832c3c0fc986093b15772
JuanChurio <est.juan.churio@unimilitar.edu.co>
2024-03-14 12:45:36 -0500

    fix html

--- src/ImageComponent.js
+++ src/ImageComponent.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import image1 from './assets/banda.png';
 import image2 from './assets/altos.png';
+import './assets/ImageComponent.css'; // Importar el CSS aquí
 
 const ImageComponent = () => {
   return (
     <div className="image-container">
+      <h2>¿Se parecen?</h2> {/* Encabezado agregado */}
       <div className="image-wrapper">
-        <img src={image1} alt="Image 1" />
-        <p>Nombre de la imagen 1</p>
+        <img src={image1} alt="Imagen 1" />
+        <p>cambio</p>
       </div>
       <div className="image-wrapper">
-        <img src={image2} alt="Image 2" />
-        <p>Nombre de la imagen 2.</p>
+        <img src={image2} alt="Imagen 2" />
+        <p>Nombre de la imagen 2</p>
       </div>
     </div>
   );
