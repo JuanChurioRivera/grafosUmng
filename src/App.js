5587f823cc97b0f78e4fbc4a11ddf5bb2f21e9ee
JuanChurio <est.juan.churio@unimilitar.edu.co>
2024-03-14 15:10:24 -0500

    redirect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ImageComponent from './ImageComponent';
-import Respuesta from './Respuesta'
-
-
+import Respuesta from './Respuesta';
 
 function App() {
-
-
   return (
-    <div className="App">
-      <Respuesta/>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<ImageComponent/>} />
+          <Route path="/respuesta" element={<Respuesta/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
