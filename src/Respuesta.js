bf79d6625bf9257211031a74af53d35e33b9b50e
JuanChurio <est.juan.churio@unimilitar.edu.co>
2024-03-14 19:02:17 -0500

    pls

--- src/Respuesta.js
+++ src/Respuesta.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'; // Importa useNavigate en lugar de useHistory
 import './assets/estiloRespuesta.css'; // Asegúrate de que el camino al CSS sea correcto
 
 const Respuesta = () => {
-  
+  const navigate = useNavigate(); // Utiliza el hook useNavigate
 
   // Función para manejar el clic en los botones
   const handleClick = () => {
-    if(onDecisionMade) {
-      onDecisionMade(); // Asegura que onDecisionMade se invoca para actualizar la combinación
-    }
-    navigate('/'); // Navega de vuelta a ImageComponent
+    navigate('/'); // Utiliza navigate para ir a la ruta de ImageComponent
   };
 
   return (
     <div>
-      <h2>porfi</h2>
+      <h2>¿Se parecen?</h2>
       {/* Añade el manejador de clics a cada botón */}
       <button className="button" onClick={handleClick}>Sí</button>
       <button className="button" onClick={handleClick}>No</button>
