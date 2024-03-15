72f9f3cdf76b126594b24485715d96e9c11a4bfa
JuanChurio <est.juan.churio@unimilitar.edu.co>
2024-03-14 19:04:54 -0500

    revert

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ImageComponent from './ImageComponent';
 import Respuesta from './Respuesta';
 
-// Función generateCombinations() aquí (sin cambios)
-
 function App() {
-  // Estado para mantener las combinaciones restantes y la actual
-  
-
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<ImageComponent combination={currentCombination} />} />
-          <Route path="/respuesta" element={<Respuesta onDecisionMade={selectNewCombination} />} />
+          <Route path="/" element={<ImageComponent/>} />
+          <Route path="/respuesta" element={<Respuesta/>} />
         </Routes>
       </div>
     </Router>
