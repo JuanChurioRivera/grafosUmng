0db802bb8ee9321deb403723c6f85d8eb1d999a0
JuanChurio <est.juan.churio@unimilitar.edu.co>
2024-03-17 14:00:41 -0500

    hotfix

--- src/Respuesta.js
+++ src/Respuesta.js
@@ -43,7 +43,7 @@ const Respuesta = () => {
         timePer: timeSpent // this should be a number
       };
       console.log("wtf")
-      const response = await axios.get('experimentdeploy.azurewebsites.net/insertRows', rowData);
+      const response = await axios.get('https://experimentdeploy.azurewebsites.net//insertRows', rowData);
       console.log(response.data);
 
       
@@ -69,7 +69,7 @@ const Respuesta = () => {
         controlCondition: ControlVar,
         timePer: timeSpent // this should be a number
       };
-      const response = await axios.get('experimentdeploy.azurewebsites.net/insertRows', rowData);
+      const response = await axios.get('https://experimentdeploy.azurewebsites.net//insertRows', rowData);
 
 
       console.log(response.data);
