d0b260b36f23cd3e45f600a8eb41e866072db1dd
JuanChurio <est.juan.churio@unimilitar.edu.co>
2024-03-20 08:31:17 -0500

    unu

--- src/Home.js
+++ src/Home.js
@@ -33,7 +33,7 @@ const Home = () => {
     // Render the component
     return (
         <div>
-            <h1>Encabezado</h1>
+            <h1>Experimento</h1>
             <div id="container">
                 <textarea
                     id="texto"
