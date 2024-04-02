44d10b3f5a565205e950678af74855fd5d20d60a
JuanChurio <est.juan.churio@unimilitar.edu.co>
2024-04-02 09:14:20 -0500

    add style

--- src/caracterizacion.js
+++ src/caracterizacion.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './assets/styleCaract.css';
 
 const Caracterizacion = () => {
     const navigate = useNavigate();
