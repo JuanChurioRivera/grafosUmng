2bb96b071085bcd215e671a15e26a0aa267d2de0
JuanChurio <est.juan.churio@unimilitar.edu.co>
2024-04-02 09:17:32 -0500

    hotfix

--- src/caracterizacion.js
+++ src/caracterizacion.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './assets/styleCaract.css';
+import './assets/styleCarac.css';
 
 const Caracterizacion = () => {
     const navigate = useNavigate();
