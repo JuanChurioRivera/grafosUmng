d27f9ec4bc20b6a1048e5701385b8419721df6ba
JuanChurio <est.juan.churio@unimilitar.edu.co>
2024-04-04 09:06:47 -0500

    hotfix

--- src/EndPrueba.js
+++ src/EndPrueba.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useCombinacion } from './CombinacionPrueba';
+import { useCombinacion } from './CombinacionContext';
 import './assets/estiloRespuesta.css';
 
 const EndPrueba = () => {
