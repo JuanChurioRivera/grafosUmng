53ccb9cc2c69b7c4a3708d26473a0ef7d94f571c
JuanChurio <est.juan.churio@unimilitar.edu.co>
2024-04-06 10:25:42 -0500

    prueba

--- src/ImagePrueba.js
+++ src/ImagePrueba.js
@@ -4,18 +4,15 @@ import './assets/style.css';
 import { useCombinacion } from './CombinacionContext';
 
 const ImagePrueba = () => {
-  const [showImage, setShowImage] = useState(true);
+  const [showImage, setShowImage] = useState(false); // Initially hide images
   const navigate = useNavigate();
   const { dataPRUEBA } = useCombinacion();
   const [primeraPalabra, segundaPalabra, terceraPalabra] = dataPRUEBA;
-  const [imagesLoaded, setImagesLoaded] = useState(false); // State to track image loading
 
   useEffect(() => {
     const images = [];
-
     const pngFileNames = ['BBB _GV.jpg', 'BBB _GVH.jpg', 'BBB _SIGNAL.jpg','CARDIOMYOPATHY_GV.jpg','CARDIOMYOPATHY_GVH.jpg','CARDIOMYOPATHY_SIGNAL.jpg','CONTROL_GV.jpg','CONTROL_GVH.jpg','CONTROL_SIGNAL.jpg','DISRHYTMIA_GV.jpg','DISRHYTMIA_GVH.jpg','DISRHYTMIA_SIGNAL.jpg','MYOCARDIAL INFARCTION _GV.jpg','MYOCARDIAL INFARCTION _GVH.jpg','MYOCARDIAL INFARCTION _SIGNAL.jpg'];
 
-    
     // Get all PNG files in the assets folder
     pngFileNames.forEach((fileName) => {
       const imagePath = `${process.env.PUBLIC_URL}/assets/${fileName}`;
@@ -23,58 +20,47 @@ const ImagePrueba = () => {
       img.src = imagePath;
       images.push(imagePath); // Store the image path in the images array
     });
+
+    setShowImage(true); // Show images after preloading
   }, []);
 
-  // Calcula las rutas de las imágenes en la carpeta public
-  let imagePath1, imagePath2;
-  if (primeraPalabra && segundaPalabra && terceraPalabra) {
-    imagePath1 = `${process.env.PUBLIC_URL}/assets/${primeraPalabra}_${terceraPalabra}.jpg`;
-    imagePath2 = `${process.env.PUBLIC_URL}/assets/${segundaPalabra}_${terceraPalabra}.jpg`;
-  }
+  const navigateAfterDelay = () => {
+    setTimeout(() => {
+      navigate('/RespuestaPrueba');
+    }, 1000);
+  };
 
   useEffect(() => {
-    if (imagesLoaded) {
-      const timer = setTimeout(() => {
-        setShowImage(false);
-        
-      }, 500); // 1000ms timer
+    if (primeraPalabra && segundaPalabra && terceraPalabra) {
+      const imagePath1 = `${process.env.PUBLIC_URL}/assets/${primeraPalabra}_${terceraPalabra}.jpg`;
+      const imagePath2 = `${process.env.PUBLIC_URL}/assets/${segundaPalabra}_${terceraPalabra}.jpg`;
 
-      return () => clearTimeout(timer);
-    }
-  }, [imagesLoaded]);
+      const img1 = new Image();
+      const img2 = new Image();
 
-  useEffect(() => {
-    if (imagesLoaded) {
-      const timer = setTimeout(() => {
-        
-        navigate('/RespuestaPrueba');
-      }, 1000);
-  
-      return () => clearTimeout(timer);
+      img1.onload = () => {
+        img2.onload = () => {
+          navigateAfterDelay();
+        };
+        img2.src = imagePath2;
+      };
+
+      img1.src = imagePath1;
     }
-  }, [imagesLoaded, navigate]);
+  }, [primeraPalabra, segundaPalabra, terceraPalabra, navigateAfterDelay]);
 
   return (
     <div className="image-container">
-      <div className="image-wrapper">
       {showImage && (
-        <img
-          src={imagePath1}
-          alt="Gráfico 2"
-          onLoad={() => setImagesLoaded(true)}
-          />
-        )}
-      </div>
-      <div className="image-wrapper">
-        {showImage && (
-          <img
-            src={imagePath2}
-            alt="Gráfico 2"
-            onLoad={() => setImagesLoaded(true)}
-          // Set imagesLoaded to true when image is loaded
-            />
-        )}
-      </div>
+        <>
+          <div className="image-wrapper">
+            <img src={`${process.env.PUBLIC_URL}/assets/${primeraPalabra}_${terceraPalabra}.jpg`} alt="Gráfico 1" />
+          </div>
+          <div className="image-wrapper">
+            <img src={`${process.env.PUBLIC_URL}/assets/${segundaPalabra}_${terceraPalabra}.jpg`} alt="Gráfico 2" />
+          </div>
+        </>
+      )}
     </div>
   );
 };
