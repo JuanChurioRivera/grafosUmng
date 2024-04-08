11e27044d57347d3b91a5d4d08cf02414e0cabd3
JuanChurio <est.juan.churio@unimilitar.edu.co>
2024-04-08 14:07:00 -0500

    uwu

--- src/ImageComponent.js
+++ src/ImageComponent.js
@@ -4,39 +4,34 @@ import './assets/style.css';
 import { useCombinacion } from './CombinacionContext';
 
 const ImageComponent = () => {
-  const [showImage, setShowImage] = useState(false); // Initially hide images
+  const [showImage, setShowImage] = useState(true);
   const navigate = useNavigate();
-  const { data,checkUpdate,setcheckUpdate } = useCombinacion();
+  const { data } = useCombinacion();
   const [primeraPalabra, segundaPalabra, terceraPalabra] = data;
+  const [imagesLoaded, setImagesLoaded] = useState(false);
 
-  useEffect(() => {
-    const images = [];
-    const pngFileNames = ['BBB _GV.jpg', 'BBB _GVH.jpg', 'BBB _SIGNAL.jpg','CARDIOMYOPATHY_GV.jpg','CARDIOMYOPATHY_GVH.jpg','CARDIOMYOPATHY_SIGNAL.jpg','CONTROL_GV.jpg','CONTROL_GVH.jpg','CONTROL_SIGNAL.jpg','DISRHYTMIA_GV.jpg','DISRHYTMIA_GVH.jpg','DISRHYTMIA_SIGNAL.jpg','MYOCARDIAL INFARCTION _GV.jpg','MYOCARDIAL INFARCTION _GVH.jpg','MYOCARDIAL INFARCTION _SIGNAL.jpg'];
+  
 
-    // Get all PNG files in the assets folder
-    pngFileNames.forEach((fileName) => {
-      const imagePath = `${process.env.PUBLIC_URL}/assets/${fileName}`;
-      const img = new Image();
-      img.src = imagePath;
-      images.push(imagePath); // Store the image path in the images array
-    });
+  
 
-     // Show images after preloading
-  }, []);
+
+  // Calcula las rutas de las imágenes en la carpeta public
+  let imagePath1, imagePath2;
+  if (primeraPalabra && segundaPalabra && terceraPalabra) {
+    imagePath1 = `${process.env.PUBLIC_URL}/assets/${primeraPalabra}_${terceraPalabra}.jpg`;
+    imagePath2 = `${process.env.PUBLIC_URL}/assets/${segundaPalabra}_${terceraPalabra}.jpg`;
+  }
 
   const navigateAfterDelay = () => {
     setTimeout(() => {
-      setcheckUpdate(false);
       navigate('/respuesta');
     }, 200);
   };
 
   useEffect(() => {
-    if (primeraPalabra && segundaPalabra && terceraPalabra && checkUpdate) {
+    if (primeraPalabra && segundaPalabra && terceraPalabra) {
       const imagePath1 = `${process.env.PUBLIC_URL}/assets/${primeraPalabra}_${terceraPalabra}.jpg`;
       const imagePath2 = `${process.env.PUBLIC_URL}/assets/${segundaPalabra}_${terceraPalabra}.jpg`;
-
-      console.log(primeraPalabra,segundaPalabra);
 
       const img1 = new Image();
       const img2 = new Image();
@@ -46,6 +41,7 @@ const ImageComponent = () => {
           setShowImage(true);
           navigateAfterDelay();
         };
+
         img2.src = imagePath2;
       };
 
@@ -55,18 +51,18 @@ const ImageComponent = () => {
 
   return (
     <div className="image-container">
-    {showImage ? (
-      <>
-        <div className="image-wrapper">
-          <img src={`${process.env.PUBLIC_URL}/assets/${primeraPalabra}_${terceraPalabra}.jpg`} alt="Gráfico 1" />
-        </div>
-        <div className="image-wrapper">
-          <img src={`${process.env.PUBLIC_URL}/assets/${segundaPalabra}_${terceraPalabra}.jpg`} alt="Gráfico 2" />
-        </div>
-      </>
-    ) : null}
-  </div>
-
+      {showImage && (
+        <>
+          <div className="image-wrapper">
+            <img src={`${process.env.PUBLIC_URL}/assets/${primeraPalabra}_${terceraPalabra}.jpg`} alt="Gráfico 1" />
+          </div>
+          <div className="image-wrapper">
+            <img src={`${process.env.PUBLIC_URL}/assets/${segundaPalabra}_${terceraPalabra}.jpg`} alt="Gráfico 2" />
+          </div>
+        </>
+      )}
+    </div>
   );
 };
+
 export default ImageComponent;
